03243598b25e7a9eac6b08c5c0115bd6f3af6924
freearhey <free.arhey@gmail.com>
2019-10-07 03:38:58 +0300

    Updated test

--- test/index.test.js
+++ test/index.test.js
@@ -5,13 +5,16 @@ it('parse playlist', () => {
     #EXTM3U x-tvg-url="http://195.154.221.171/epg/guidealbania.xml.gz"
     #EXTINF:-1 tvg-ID="CH1" tvg-name="Ch 1" tvg-logo="https://i.imgur.com/LcJZVV5.png" tvg-url="http://195.154.221.171/epg/guide.xml.gz" group-title="Music",Channel 1 (Tested)
     http://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8
-    #EXTINF:0 tvg-id="CH2",Channel 2
+    #EXTINF:16 tvg-id="CH2",Channel 2
     stream/chunklist.m3u8
   `
 
   expect(parser.parse(playlist)).toStrictEqual({
-    tvg: {
-      url: 'http://195.154.221.171/epg/guidealbania.xml.gz'
+    header: {
+      attrs: {
+        'x-tvg-url': 'http://195.154.221.171/epg/guidealbania.xml.gz'
+      },
+      raw: '#EXTM3U x-tvg-url="http://195.154.221.171/epg/guidealbania.xml.gz"'
     },
     items: [
       { 
@@ -25,7 +28,8 @@ it('parse playlist', () => {
         group: {
           title: 'Music'
         },
-        url: 'http://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8'
+        url: 'http://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8',
+        raw: '#EXTINF:-1 tvg-ID="CH1" tvg-name="Ch 1" tvg-logo="https://i.imgur.com/LcJZVV5.png" tvg-url="http://195.154.221.171/epg/guide.xml.gz" group-title="Music",Channel 1 (Tested)\nhttp://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8'
       },
       { 
         name: 'Channel 2',
@@ -38,7 +42,8 @@ it('parse playlist', () => {
         group: {
           title: ''
         },
-        url: 'stream/chunklist.m3u8'
+        url: 'stream/chunklist.m3u8',
+        raw: '#EXTINF:16 tvg-id="CH2",Channel 2\nstream/chunklist.m3u8'
       }
     ]
   })
