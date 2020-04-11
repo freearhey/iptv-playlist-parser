b6e6cf876cd26ab3624258da066c8f8d19c2ace7
freearhey <free.arhey@gmail.com>
2020-04-11 18:04:43 +0300

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -4,9 +4,12 @@ it('parse playlist', () => {
   const playlist = `
     #EXTM3U x-tvg-url="http://195.154.221.171/epg/guidealbania.xml.gz"
     #EXTINF:-1 tvg-ID="CH1" tvg-name="Ch 1" tvg-language="English" tvg-country="US" tvg-logo="https://i.imgur.com/LcJZVV5.png" tvg-url="http://195.154.221.171/epg/guide.xml.gz" group-title="Music",Channel 1 (Tested)
+    #EXTVLCOPT:http-referrer=http://player.livesports.pw/la2/
+    #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.25 Safari/537.36
     http://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8
     #EXTINF:16 tvg-id="CH2",Channel 2
-    stream/chunklist.m3u8
+stream/chunklist.m3u8
+
   `
 
   expect(parser.parse(playlist)).toStrictEqual({
@@ -30,10 +33,16 @@ it('parse playlist', () => {
         group: {
           title: 'Music'
         },
-        url:
-          'http://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8',
-        raw:
-          '#EXTINF:-1 tvg-id="CH1" tvg-name="Ch 1" tvg-language="English" tvg-country="US" tvg-logo="https://i.imgur.com/LcJZVV5.png" tvg-url="http://195.154.221.171/epg/guide.xml.gz" group-title="Music",Channel 1 (Tested)\nhttp://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8'
+        http: {
+          referrer: 'http://player.livesports.pw/la2/',
+          'user-agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.25 Safari/537.36'
+        },
+        url: 'http://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8',
+        raw: `#EXTINF:-1 tvg-ID="CH1" tvg-name="Ch 1" tvg-language="English" tvg-country="US" tvg-logo="https://i.imgur.com/LcJZVV5.png" tvg-url="http://195.154.221.171/epg/guide.xml.gz" group-title="Music",Channel 1 (Tested)
+    #EXTVLCOPT:http-referrer=http://player.livesports.pw/la2/
+    #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.25 Safari/537.36
+    http://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8`
       },
       {
         name: 'Channel 2',
@@ -47,6 +56,10 @@ it('parse playlist', () => {
         },
         group: {
           title: ''
+        },
+        http: {
+          referrer: '',
+          'user-agent': ''
         },
         url: 'stream/chunklist.m3u8',
         raw: '#EXTINF:16 tvg-id="CH2",Channel 2\nstream/chunklist.m3u8'
@@ -66,7 +79,8 @@ http://cdn-hls.globecast.tv/live/ramdisk/tamazight_tv8_snrt/hls_snrt/index.m3u8
   expect(parser.parse(playlist)).toStrictEqual({
     header: {
       attrs: {},
-      raw: '#EXTM3U'
+      raw:
+        '#EXTM3U\n================morocco================================================================'
     },
     items: [
       {
@@ -82,8 +96,11 @@ http://cdn-hls.globecast.tv/live/ramdisk/tamazight_tv8_snrt/hls_snrt/index.m3u8
         group: {
           title: ''
         },
-        url:
-          'http://cdn-hls.globecast.tv/live/ramdisk/tamazight_tv8_snrt/hls_snrt/index.m3u8',
+        http: {
+          referrer: '',
+          'user-agent': ''
+        },
+        url: 'http://cdn-hls.globecast.tv/live/ramdisk/tamazight_tv8_snrt/hls_snrt/index.m3u8',
         raw:
           '#EXTINF:-1,tamazight tv8\nhttp://cdn-hls.globecast.tv/live/ramdisk/tamazight_tv8_snrt/hls_snrt/index.m3u8'
       }
