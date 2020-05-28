df5a0de964d947f6d3b73a9b920bafbbb08092db
freearhey <free.arhey@gmail.com>
2020-05-28 14:47:59 +0300

    Updated test example
    URL may contain single quotation mark

--- test/index.test.js
+++ test/index.test.js
@@ -16,15 +16,15 @@ stream/chunklist.m3u8
 
 http://wodpro24.tr4545.in:8080/expred/peru8k.mp4
 #EXTINF:1,TRT 1 HD
-https://trtcanlitv-lh.akamaihd.net/i/TRT1HD_1@181842/index_1550_av-p.m3u8?sd=10&rebase=on
+https://video-rvd-lmg.rnp.br/live/ocp(t(FfZfeFx3QG4)r(TOqkzw)a(ut273w)p(d(lCo)k(ow4)m(U1zbeMZOcvuVp_PjPC5VeA)n(a(-xL_Aw)s(yeg)'a(vuk2sg)s(3Rc)))s(s(2h8)b(OVsX9brZ0psZeMaQYt9O46THOxFsPHFB6R_YrLO_lAYLehFKlBcB3utUOhZjyWY7C52ngNY4Ow9Ook29TA)'s(gRY)b(iE9B80a6sOKUZn0UnLJQU_bKwqIDP29GWBxvJz1bQ1ihEmrOuDaUGTinW0l8unt4U9_v4jCwiAC2T6TOa8pgVRIAfsnYG9Y7VoI5EmwQ)'s(A4Q)b(ayZ0dwLn))m(0))/index.m3u8
   `
 
   expect(parser.parse(playlist)).toStrictEqual({
     header: {
       attrs: {
-        'x-tvg-url': 'http://195.154.221.171/epg/guidealbania.xml.gz',
+        'x-tvg-url': 'http://195.154.221.171/epg/guidealbania.xml.gz'
       },
-      raw: '#EXTM3U x-tvg-url="http://195.154.221.171/epg/guidealbania.xml.gz"',
+      raw: '#EXTM3U x-tvg-url="http://195.154.221.171/epg/guidealbania.xml.gz"'
     },
     items: [
       {
@@ -36,19 +36,19 @@ https://trtcanlitv-lh.akamaihd.net/i/TRT1HD_1@181842/index_1550_av-p.m3u8?sd=10&
           country: 'US',
           logo:
             'http://www.rtvchannel.com.au/wp-content/uploads/2017/04/xshow_08.png,Mic_.2KNN9OHw1p.png.pagespeed.ce.2KNN9OHw1p.png',
-          url: 'http://195.154.221.171/epg/guide.xml.gz',
+          url: 'http://195.154.221.171/epg/guide.xml.gz'
         },
         group: {
-          title: 'Music',
+          title: 'Music'
         },
         http: {
           referrer: 'http://player.livesports.pw/la2/',
           'user-agent':
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.25 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.25 Safari/537.36'
         },
         url: 'http://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8',
         raw:
-          '#EXTINF:-1 tvg-ID="CH1" tvg-name="Ch 1" tvg-language="English" tvg-country="US" tvg-logo="http://www.rtvchannel.com.au/wp-content/uploads/2017/04/xshow_08.png,Mic_.2KNN9OHw1p.png.pagespeed.ce.2KNN9OHw1p.png" tvg-url="http://195.154.221.171/epg/guide.xml.gz" group-title="Music",Channel 1 (Tested)\n    #EXTVLCOPT:http-referrer=http://player.livesports.pw/la2/\n    #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.25 Safari/537.36\n    http://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8',
+          '#EXTINF:-1 tvg-ID="CH1" tvg-name="Ch 1" tvg-language="English" tvg-country="US" tvg-logo="http://www.rtvchannel.com.au/wp-content/uploads/2017/04/xshow_08.png,Mic_.2KNN9OHw1p.png.pagespeed.ce.2KNN9OHw1p.png" tvg-url="http://195.154.221.171/epg/guide.xml.gz" group-title="Music",Channel 1 (Tested)\n    #EXTVLCOPT:http-referrer=http://player.livesports.pw/la2/\n    #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.25 Safari/537.36\n    http://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8'
       },
       {
         name: 'Channel 2',
@@ -58,17 +58,17 @@ https://trtcanlitv-lh.akamaihd.net/i/TRT1HD_1@181842/index_1550_av-p.m3u8?sd=10&
           language: '',
           country: '',
           logo: '',
-          url: '',
+          url: ''
         },
         group: {
-          title: '',
+          title: ''
         },
         http: {
           referrer: '',
-          'user-agent': '',
+          'user-agent': ''
         },
         url: 'stream/chunklist.m3u8',
-        raw: `#EXTINF:16 tvg-id="CH2",Channel 2\nstream/chunklist.m3u8\n\n=========================sudan===================================\n#EXTM3U`,
+        raw: `#EXTINF:16 tvg-id="CH2",Channel 2\nstream/chunklist.m3u8\n\n=========================sudan===================================\n#EXTM3U`
       },
       {
         name: '=>(★ TURKİYE ★)<=',
@@ -78,17 +78,17 @@ https://trtcanlitv-lh.akamaihd.net/i/TRT1HD_1@181842/index_1550_av-p.m3u8?sd=10&
           language: '',
           country: '',
           logo: '',
-          url: '',
+          url: ''
         },
         group: {
-          title: '',
+          title: ''
         },
         http: {
           referrer: '',
-          'user-agent': '',
+          'user-agent': ''
         },
         url: 'http://wodpro24.tr4545.in:8080/expred/peru8k.mp4',
-        raw: `#EXTINF:1,=>(★ TURKİYE ★)<=\n\nhttp://wodpro24.tr4545.in:8080/expred/peru8k.mp4`,
+        raw: `#EXTINF:1,=>(★ TURKİYE ★)<=\n\nhttp://wodpro24.tr4545.in:8080/expred/peru8k.mp4`
       },
       {
         name: 'TRT 1 HD',
@@ -98,20 +98,20 @@ https://trtcanlitv-lh.akamaihd.net/i/TRT1HD_1@181842/index_1550_av-p.m3u8?sd=10&
           language: '',
           country: '',
           logo: '',
-          url: '',
+          url: ''
         },
         group: {
-          title: '',
+          title: ''
         },
         http: {
           referrer: '',
-          'user-agent': '',
+          'user-agent': ''
         },
         url:
-          'https://trtcanlitv-lh.akamaihd.net/i/TRT1HD_1@181842/index_1550_av-p.m3u8?sd=10&rebase=on',
-        raw: `#EXTINF:1,TRT 1 HD\nhttps://trtcanlitv-lh.akamaihd.net/i/TRT1HD_1@181842/index_1550_av-p.m3u8?sd=10&rebase=on`,
-      },
-    ],
+          "https://video-rvd-lmg.rnp.br/live/ocp(t(FfZfeFx3QG4)r(TOqkzw)a(ut273w)p(d(lCo)k(ow4)m(U1zbeMZOcvuVp_PjPC5VeA)n(a(-xL_Aw)s(yeg)'a(vuk2sg)s(3Rc)))s(s(2h8)b(OVsX9brZ0psZeMaQYt9O46THOxFsPHFB6R_YrLO_lAYLehFKlBcB3utUOhZjyWY7C52ngNY4Ow9Ook29TA)'s(gRY)b(iE9B80a6sOKUZn0UnLJQU_bKwqIDP29GWBxvJz1bQ1ihEmrOuDaUGTinW0l8unt4U9_v4jCwiAC2T6TOa8pgVRIAfsnYG9Y7VoI5EmwQ)'s(A4Q)b(ayZ0dwLn))m(0))/index.m3u8",
+        raw: `#EXTINF:1,TRT 1 HD\nhttps://video-rvd-lmg.rnp.br/live/ocp(t(FfZfeFx3QG4)r(TOqkzw)a(ut273w)p(d(lCo)k(ow4)m(U1zbeMZOcvuVp_PjPC5VeA)n(a(-xL_Aw)s(yeg)'a(vuk2sg)s(3Rc)))s(s(2h8)b(OVsX9brZ0psZeMaQYt9O46THOxFsPHFB6R_YrLO_lAYLehFKlBcB3utUOhZjyWY7C52ngNY4Ow9Ook29TA)'s(gRY)b(iE9B80a6sOKUZn0UnLJQU_bKwqIDP29GWBxvJz1bQ1ihEmrOuDaUGTinW0l8unt4U9_v4jCwiAC2T6TOa8pgVRIAfsnYG9Y7VoI5EmwQ)'s(A4Q)b(ayZ0dwLn))m(0))/index.m3u8`
+      }
+    ]
   })
 })
 
@@ -127,7 +127,7 @@ http://cdn-hls.globecast.tv/live/ramdisk/tamazight_tv8_snrt/hls_snrt/index.m3u8
     header: {
       attrs: {},
       raw:
-        '#EXTM3U\n================morocco================================================================',
+        '#EXTM3U\n================morocco================================================================'
     },
     items: [
       {
@@ -138,19 +138,19 @@ http://cdn-hls.globecast.tv/live/ramdisk/tamazight_tv8_snrt/hls_snrt/index.m3u8
           language: '',
           country: '',
           logo: '',
-          url: '',
+          url: ''
         },
         group: {
-          title: '',
+          title: ''
         },
         http: {
           referrer: '',
-          'user-agent': '',
+          'user-agent': ''
         },
         url: 'http://cdn-hls.globecast.tv/live/ramdisk/tamazight_tv8_snrt/hls_snrt/index.m3u8',
         raw:
-          '#EXTINF:-1,tamazight tv8\nhttp://cdn-hls.globecast.tv/live/ramdisk/tamazight_tv8_snrt/hls_snrt/index.m3u8',
-      },
-    ],
+          '#EXTINF:-1,tamazight tv8\nhttp://cdn-hls.globecast.tv/live/ramdisk/tamazight_tv8_snrt/hls_snrt/index.m3u8'
+      }
+    ]
   })
 })
