0d97241fdbaf1d9283a375d2a27d4bf316d7d8c4
Aleksandr Statciuk <free.arhey@gmail.com>
2021-05-19 15:35:36 +0300

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -39,7 +39,7 @@ https://video-rvd-lmg.rnp.br/live/ocp(t(FfZfeFx3QG4)r(TOqkzw)a(ut273w)p(d(lCo)k(
           logo:
             'http://www.rtvchannel.com.au/wp-content/uploads/2017/04/xshow_08.png,Mic_.2KNN9OHw1p.png.pagespeed.ce.2KNN9OHw1p.png',
           url: 'http://195.154.221.171/epg/guide.xml.gz',
-          rec: "3"
+          rec: '3'
         },
         group: {
           title: 'Только Android'
@@ -52,12 +52,12 @@ https://video-rvd-lmg.rnp.br/live/ocp(t(FfZfeFx3QG4)r(TOqkzw)a(ut273w)p(d(lCo)k(
         url: 'http://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8',
         raw:
           '#EXTINF:-1 tvg-ID="CH1" tvg-name="Ch 1" tvg-language="English" tvg-country="US" tvg-logo="http://www.rtvchannel.com.au/wp-content/uploads/2017/04/xshow_08.png,Mic_.2KNN9OHw1p.png.pagespeed.ce.2KNN9OHw1p.png" tvg-url="http://195.154.221.171/epg/guide.xml.gz" tvg-rec="3" group-title="Music",Channel 1 (Tested)\n    #EXTGRP:Только Android\n    #EXTVLCOPT:http-referrer=http://player.livesports.pw/la2/\n    #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.25 Safari/537.36\n    http://livestream.htp.tv/hls-live/livepkgr/_definst_/H1/H1_HQ.m3u8',
-          timeshift: '',
-          catchup: {
-            type: '',
-            source: '',
-            days: ''
-          }
+        timeshift: '',
+        catchup: {
+          type: '',
+          source: '',
+          days: ''
+        }
       },
       {
         name: 'Channel 2',
@@ -80,11 +80,11 @@ https://video-rvd-lmg.rnp.br/live/ocp(t(FfZfeFx3QG4)r(TOqkzw)a(ut273w)p(d(lCo)k(
         url: 'stream/chunklist.m3u8',
         raw: `#EXTINF:16 tvg-id="CH2",Channel 2\nstream/chunklist.m3u8\n\n=========================sudan===================================\n#EXTM3U`,
         timeshift: '',
-          catchup: {
-            type: '',
-            source: '',
-            days: ''
-          }
+        catchup: {
+          type: '',
+          source: '',
+          days: ''
+        }
       },
       {
         name: '=>(★ TURKİYE ★)<=',
@@ -107,11 +107,11 @@ https://video-rvd-lmg.rnp.br/live/ocp(t(FfZfeFx3QG4)r(TOqkzw)a(ut273w)p(d(lCo)k(
         url: 'http://wodpro24.tr4545.in:8080/expred/peru8k.mp4',
         raw: `#EXTINF:1,=>(★ TURKİYE ★)<=\n\nhttp://wodpro24.tr4545.in:8080/expred/peru8k.mp4`,
         timeshift: '',
-          catchup: {
-            type: '',
-            source: '',
-            days: ''
-          }
+        catchup: {
+          type: '',
+          source: '',
+          days: ''
+        }
       },
       {
         name: 'TRT 1 HD',
@@ -135,11 +135,11 @@ https://video-rvd-lmg.rnp.br/live/ocp(t(FfZfeFx3QG4)r(TOqkzw)a(ut273w)p(d(lCo)k(
           "https://video-rvd-lmg.rnp.br/live/ocp(t(FfZfeFx3QG4)r(TOqkzw)a(ut273w)p(d(lCo)k(ow4)m(U1zbeMZOcvuVp_PjPC5VeA)n(a(-xL_Aw)s(yeg)'a(vuk2sg)s(3Rc)))s(s(2h8)b(OVsX9brZ0psZeMaQYt9O46THOxFsPHFB6R_YrLO_lAYLehFKlBcB3utUOhZjyWY7C52ngNY4Ow9Ook29TA)'s(gRY)b(iE9B80a6sOKUZn0UnLJQU_bKwqIDP29GWBxvJz1bQ1ihEmrOuDaUGTinW0l8unt4U9_v4jCwiAC2T6TOa8pgVRIAfsnYG9Y7VoI5EmwQ)'s(A4Q)b(ayZ0dwLn))m(0))/index.m3u8",
         raw: `#EXTINF:1 timeshift="3" catchup="shift" catchup-days="3" catchup-source="https://m3u-server/hls-apple-s4-c494-abcdef.m3u8?utc=325234234&lutc=3123125324",TRT 1 HD\nhttps://video-rvd-lmg.rnp.br/live/ocp(t(FfZfeFx3QG4)r(TOqkzw)a(ut273w)p(d(lCo)k(ow4)m(U1zbeMZOcvuVp_PjPC5VeA)n(a(-xL_Aw)s(yeg)'a(vuk2sg)s(3Rc)))s(s(2h8)b(OVsX9brZ0psZeMaQYt9O46THOxFsPHFB6R_YrLO_lAYLehFKlBcB3utUOhZjyWY7C52ngNY4Ow9Ook29TA)'s(gRY)b(iE9B80a6sOKUZn0UnLJQU_bKwqIDP29GWBxvJz1bQ1ihEmrOuDaUGTinW0l8unt4U9_v4jCwiAC2T6TOa8pgVRIAfsnYG9Y7VoI5EmwQ)'s(A4Q)b(ayZ0dwLn))m(0))/index.m3u8`,
         timeshift: '3',
-          catchup: {
-            type: 'shift',
-            source: 'https://m3u-server/hls-apple-s4-c494-abcdef.m3u8?utc=325234234&lutc=3123125324',
-            days: '3'
-          }
+        catchup: {
+          type: 'shift',
+          source: 'https://m3u-server/hls-apple-s4-c494-abcdef.m3u8?utc=325234234&lutc=3123125324',
+          days: '3'
+        }
       },
       {
         name: ' name',
@@ -162,11 +162,11 @@ https://video-rvd-lmg.rnp.br/live/ocp(t(FfZfeFx3QG4)r(TOqkzw)a(ut273w)p(d(lCo)k(
         url: 'http://100.101.102.103:1000/index.m3u8',
         raw: `#EXTINF: -1, name\r\nhttp://100.101.102.103:1000/index.m3u8`,
         timeshift: '',
-          catchup: {
-            type: '',
-            source: '',
-            days: ''
-          }
+        catchup: {
+          type: '',
+          source: '',
+          days: ''
+        }
       }
     ]
   })
