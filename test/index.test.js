4dd4190d8b6c76ff26c17c383d8191797559b9fa
Aleksandr Statciuk <github@oiya.xyz>
2021-12-09 08:13:40 +0300

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,7 @@
 import parser from '../src'
 
 it('parse playlist', () => {
-  const playlist = `
-    #EXTM3U x-tvg-url="http://195.154.221.171/epg/guidealbania.xml.gz"
+  const playlist = `#EXTM3U x-tvg-url="http://195.154.221.171/epg/guidealbania.xml.gz"
     #EXTINF:-1 tvg-ID="CH1" tvg-name="Ch 1" tvg-language="English" tvg-country="US" tvg-logo="http://www.rtvchannel.com.au/wp-content/uploads/2017/04/xshow_08.png,Mic_.2KNN9OHw1p.png.pagespeed.ce.2KNN9OHw1p.png" tvg-url="http://195.154.221.171/epg/guide.xml.gz" tvg-rec="3" group-title="Music",Channel 1 (Tested)
     #EXTGRP:Только Android
     #EXTVLCOPT:http-referrer=http://player.livesports.pw/la2/
@@ -208,7 +207,7 @@ http://cdn-hls.globecast.tv/live/ramdisk/tamazight_tv8_snrt/hls_snrt/index.m3u8
         },
         url: 'http://cdn-hls.globecast.tv/live/ramdisk/tamazight_tv8_snrt/hls_snrt/index.m3u8',
         raw: '#EXTINF:-1,tamazight tv8\nhttp://cdn-hls.globecast.tv/live/ramdisk/tamazight_tv8_snrt/hls_snrt/index.m3u8',
-        line: 3,
+        line: 4,
         timeshift: '',
         catchup: {
           type: '',
@@ -254,7 +253,7 @@ http://test.channel.com/iptv/secret/1/index.m3u8`
         },
         url: 'http://test.channel.com/iptv/secret/1/index.m3u8',
         raw: '#EXTINF:0 tvg-name="TestChannel" group-title="Entertainment",Test Channel\n#EXTGRP:News\n#EXTVLCOPT:http-user-agent="Dalvik/2.1.0 (Linux; U; Android 6.0.1;)"\nhttp://test.channel.com/iptv/secret/1/index.m3u8',
-        line: 2,
+        line: 3,
         timeshift: '',
         catchup: {
           type: '',
@@ -299,7 +298,7 @@ https://agromais/playlist.m3u8
         },
         url: 'https://agromais/playlist.m3u8',
         raw: `#EXTINF:-1 tvg-id="Agromais.br" user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148" group-title="Entertainment",Agromais\nhttps://agromais/playlist.m3u8`,
-        line: 2,
+        line: 3,
         timeshift: '',
         catchup: {
           type: '',
@@ -347,7 +346,7 @@ http://my-news-stream:8999/d/letsplay/video.mp4|Referer=http://test.abc/blank.ht
         },
         url: 'http://192.54.104.122:8080/d/abcdef/video.mp4',
         raw: `#EXTINF:17 tvg-id="SportChannel",Sport channel\nhttp://192.54.104.122:8080/d/abcdef/video.mp4|User-Agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko&Referer=http://180upload.abc/embed-uaqmgcckhpcl.html`,
-        line: 2,
+        line: 3,
         timeshift: '',
         catchup: {
           type: '',
@@ -376,7 +375,7 @@ http://my-news-stream:8999/d/letsplay/video.mp4|Referer=http://test.abc/blank.ht
         },
         url: 'http://my-news-stream:8999/d/letsplay/video.mp4',
         raw: `#EXTINF:555 tvg-id="News99",News channel\n#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.25 Safari/537.36\nhttp://my-news-stream:8999/d/letsplay/video.mp4|Referer=http://test.abc/blank.html`,
-        line: 4,
+        line: 5,
         timeshift: '',
         catchup: {
           type: '',
@@ -423,7 +422,7 @@ https://agromais/playlist.m3u8
         },
         url: 'https://agromais/playlist.m3u8',
         raw: `#EXTINF:-1 tvg-id="Agromais.br" user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148" group-title="Entertainment",Agromais\nhttps://agromais/playlist.m3u8`,
-        line: 2,
+        line: 3,
         timeshift: '',
         catchup: {
           type: '',
@@ -472,7 +471,7 @@ https://agromais/playlist.m3u8
         },
         url: 'https://agromais/playlist.m3u8',
         raw: `#EXTINF:-1 tvg-id="Agromais.br" user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148" group-title="Entertainment",Agromais\nhttps://agromais/playlist.m3u8`,
-        line: 2,
+        line: 3,
         timeshift: '',
         catchup: {
           type: '',
@@ -501,7 +500,7 @@ https://agromais/playlist.m3u8
         },
         url: 'https://agromais/playlist.m3u8',
         raw: `#EXTINF:-1 tvg-id="Agromais.br" user-agent="Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148" group-title="Entertainment",Agromais\nhttps://agromais/playlist.m3u8`,
-        line: 4,
+        line: 5,
         timeshift: '',
         catchup: {
           type: '',
