cbe2d73bc5cd52dd08337b351345c08f322ccba2
freearhey <7253922+freearhey@users.noreply.github.com>
2025-07-10 17:15:52 +0300

    Update tests

--- tests/__data__/expected/kodi.js
+++ tests/__data__/expected/kodi.js
@@ -23,7 +23,7 @@ export default {
         referrer: '',
         'user-agent': 'RTP Play/2.0.26 (Linux;Android 11) ExoPlayerLib/2.11.8'
       },
-      url: 'https://streaming-live.rtp.pt/liverepeater/rtpClean1HD.smil/playlist.m3u8',
+      url: 'https://streaming-live.rtp.pt/liverepeater/rtpClean1HD.smil/playlist.m3u8|user-agent=RTP Play/2.0.26 (Linux;Android 11) ExoPlayerLib/2.11.8',
       raw: `#EXTINF:-1 group-title="TV" tvg-id="rtp1hd.tv.vodafone.pt" tvg-logo="https://raw.githubusercontent.com/LITUATUI/M3UPT/main/logos/RTP1.png",RTP 1\r\n#KODIPROP:inputstream=inputstream.adaptive\r\n#KODIPROP:inputstream.adaptive.manifest_type=hls\r\nhttps://streaming-live.rtp.pt/liverepeater/rtpClean1HD.smil/playlist.m3u8|user-agent=RTP Play/2.0.26 (Linux;Android 11) ExoPlayerLib/2.11.8`,
       line: 7,
       timeshift: '',
--- tests/__data__/expected/pipe.js
+++ tests/__data__/expected/pipe.js
@@ -21,7 +21,7 @@ export default {
         referrer: 'http://180upload.abc/embed-uaqmgcckhpcl.html',
         'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko'
       },
-      url: 'http://192.54.104.122:8080/d/abcdef/video.mp4',
+      url: 'http://192.54.104.122:8080/d/abcdef/video.mp4|User-Agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko&Referer=http://180upload.abc/embed-uaqmgcckhpcl.html',
       raw: `#EXTINF:17 tvg-id="SportChannel",Sport channel\r\nhttp://192.54.104.122:8080/d/abcdef/video.mp4|User-Agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko&Referer=http://180upload.abc/embed-uaqmgcckhpcl.html`,
       line: 2,
       timeshift: '',
@@ -50,7 +50,7 @@ export default {
         'user-agent':
           'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.25 Safari/537.36'
       },
-      url: 'http://my-news-stream:8999/d/letsplay/video.mp4',
+      url: 'http://my-news-stream:8999/d/letsplay/video.mp4|Referer=http://test.abc/blank.html',
       raw: `#EXTINF:555 tvg-id="News99",News channel\r\n#EXTVLCOPT:http-user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.25 Safari/537.36\r\nhttp://my-news-stream:8999/d/letsplay/video.mp4|Referer=http://test.abc/blank.html`,
       line: 4,
       timeshift: '',
